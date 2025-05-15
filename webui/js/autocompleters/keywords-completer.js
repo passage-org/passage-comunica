88ce3166368b1d76eb5dae3a61eeeaeca7b0c03f
chat-wane <grumpy.chat.wane@gmail.com>
2025-05-15 17:07:08 +0200

    added all keywords from sparql1.1, sort by prefix depth; fix the error indicator over query button

--- webui/js/autocompleters/keywords-completer.js
+++ webui/js/autocompleters/keywords-completer.js
@@ -5,12 +5,26 @@ export const KeywordsCompleter = {
     // ugly, would be better coming from a dedicated package such as
     // sparqlalgebra or even in yasgui, but seems complicated to retrieve
     // them, whereas the set of keywords is easily writable.
-    
-    keywords : ['SELECT','DISTINCT','CONSTRUCT','DESCRIBE','ASK','WHERE',
-                'FROM','NAMED','PREFIX','BASE','OPTIONAL','FILTER',
-                'GRAPH','LIMIT','OFFSET','SERVICE','UNION','EXISTS','NOT',
-                'BINDINGS','MINUS','ORDER','BY','VALUES','ISIRI','IF','STR',
-                'REPLACE','AS','URI','BIND','COUNT','GROUP','DESC','ASC','A'],
+    // comes from : https://www.w3.org/TR/sparql11-query/#rQuery
+    keywords : ['PREFIX', 'BASE',
+                'SELECT','CONSTRUCT','DESCRIBE','ASK',
+                'FROM', 'NAMED',
+                'DISTINCT', 'WHERE',
+                'OPTIONAL','FILTER',
+                'GRAPH','LIMIT','OFFSET','SERVICE','SILENT','UNION','EXISTS','NOT',
+                'MINUS','ORDER','BY','VALUES','IF',
+                'REPLACE','AS','BIND','GROUP','DESC','ASC','A',
+                'COUNT', 'SUM', 'AVG', 'MIN', 'MAX', 'SAMPLE', 'GROUP_CONCAT',
+                'COALESCE', 'IN', 'HAVING',
+                'LANG', 'LANGMATCHES', 'DATATYPE', 'BOUND', 'IRI', 'URI',
+                'BNODE', 'RAND', 'STR', 'ABS', 'CEIL', 'FLOOR', 'ROUND',
+                'CONCAT', 'STRLEN', 'UCASE', 'LCASE', 'ENCODE_FOR_URI', 'CONTAINS',
+                'STRSTARTS', 'STRENDS', 'STRBEFORE', 'STRAFTER', 'YEAR', 'MONTH',
+                'DAY', 'HOURS', 'MINUTES', 'SECONDS', 'TIMEZONE', 'TZ', 'NOW',
+                'UUID', 'STRUUID', 'MD5', 'SHA1', 'SHA256', 'SHA384', 'SHA512',
+                'STRLANG', 'STRDT', 'SAMETERM',
+                'ISIRI', 'ISURI', 'ISBLANK', 'ISLITERAL', 'ISNUMERIC',
+                'SUBSTR', 'REGEX'],
     
     name: "keywords-completer",
     autoShow: true,
@@ -32,7 +46,10 @@ export const KeywordsCompleter = {
     },
 
     /// returns the list of keywords that are possible depending on what's being typed.
+    /// The list is sorted by the depth of the match.
     getPossibleKeywords(typed, possibilities) {
-        return possibilities.filter(p => p.includes(typed.toUpperCase()) && this.keywords.includes(p.toUpperCase()));
+        const upperCased = typed.toUpperCase();
+        return possibilities.filter(p => p.includes(upperCased) && this.keywords.includes(p.toUpperCase()))
+            .sort((a, b) => {return a.indexOf(upperCased) - b.indexOf(upperCased);});
     },
 };
