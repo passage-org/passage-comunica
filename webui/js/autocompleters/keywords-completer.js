11c47e90613b0aa14b24d85b852bd65843f01f8e
chat-wane <grumpy.chat.wane@gmail.com>
2025-05-15 16:24:26 +0200

    started an 'about' graph of triples to describe the web editor; created a keyword autocompleter

--- webui/js/autocompleters/keywords-completer.js
+++ webui/js/autocompleters/keywords-completer.js
@@ -2,19 +2,37 @@
 /// When a keywords is started, it allows autocompleting based on typed
 /// characters, but also filter by possible token at cursor position.
 export const KeywordsCompleter = {
+    // ugly, would be better coming from a dedicated package such as
+    // sparqlalgebra or even in yasgui, but seems complicated to retrieve
+    // them, whereas the set of keywords is easily writable.
+    
+    keywords : ['SELECT','DISTINCT','CONSTRUCT','DESCRIBE','ASK','WHERE',
+                'FROM','NAMED','PREFIX','BASE','OPTIONAL','FILTER',
+                'GRAPH','LIMIT','OFFSET','SERVICE','UNION','EXISTS','NOT',
+                'BINDINGS','MINUS','ORDER','BY','VALUES','ISIRI','IF','STR',
+                'REPLACE','AS','URI','BIND','COUNT','GROUP','DESC','ASC','A'],
+    
     name: "keywords-completer",
     autoShow: true,
+    bulk: false, // press <ctrl-space> to autocomplete
+    
     get: function(yasqe, token) {
-        // TODO
-        return Promise.resolve(['https://ex1.org', 'https://ex2.org'])
+        if (!token) {return Promise.resolve([]);}; // <= mostly important for bulk, but we keep it
+        return Promise.resolve(this.getPossibleKeywords(token.string, token.state.possibleCurrent));
     },
+    
     isValidCompletionPosition: function (yasqe) {
-        // TODO TODO
         const token = yasqe.getCompleteToken();
-        console.log(token);
-        if (token.string.length == 0) return false; //we want -something- to autocomplete
-        if (token.string[0] === "?" || token.string[0] === "$") return false; // we are typing a var
-        if (token.state.possibleCurrent.indexOf("IRI_REF") >= 0) return true; // predicate pos
-        return false;
+        if (token.string.length === 0) { return false };
+        const possibilities = this.getPossibleKeywords(token.string, token.state.possibleCurrent);
+        if (possibilities.length === 1 && possibilities[0] === token.string) { // exact match already
+            return false;
+        }
+        return possibilities.length > 0;
+    },
+
+    /// returns the list of keywords that are possible depending on what's being typed.
+    getPossibleKeywords(typed, possibilities) {
+        return possibilities.filter(p => p.includes(typed.toUpperCase()) && this.keywords.includes(p.toUpperCase()));
     },
 };
---
+++ webui/js/views/about-graph.js
@@ -0,0 +1,18 @@
+
+/// Display all the information about the version that
+/// built the system. It should be statically generated, or at least call
+/// on package.json to fill some of the information, like versions.
+export const AboutGraph = {
+    ['<client>',           '<software>', '<yasgui>'],
+    ['<client>',           '<software>', '<passage-communica>'],
+    ['<comunica>',         '<version>',  '4.2.0'],
+    ['<comunica>',         '<webpage>',  '<https://comunica.dev/>'],
+    ['<passage>',          '<version>',  '0.0.3'],
+    ['<passage>',          '<webpage>',  '<https://github.com/passage-org>'],
+    ['<passage-comunica>', '<extend>',   '<comunica>']
+    ['<passage-comunica>', '<version>',  '0.0.3'],
+    ['<passage-comunica>', '<webpage>',  '<https://passage-org.github.io/passage-comunica/>'],
+    ['<server>',           '<software>', '<passage>'],
+    ['<yasgui>',           '<version>',  '4.2.28'],
+    ['<yasgui>',           '<webpage>',  'https://yasgui.triply.cc/'],
+}
