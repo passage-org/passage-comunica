51e81d1a5fe8e96508cf60540e7d310ef617dc8b
chat-wane <grumpy.chat.wane@gmail.com>
2025-11-17 15:19:41 +0100

    basic tests to make sure that shapes accept what it needs to accept

--- packages/actor-query-source-identify-hypermedia-passage/test/Shapes-test.ts
+++ packages/actor-query-source-identify-hypermedia-passage/test/Shapes-test.ts
@@ -19,6 +19,65 @@ describe('Shapes', () => {
         translate('SELECT * WHERE { ?person <http://own> ?animal . ?animal <http://species> ?species }')
       )).toBeTruthy();
     })
+
+    it('should transform the sequence path so it gets accepted', () => {
+      expect(doesShapeAcceptOperation(
+        Shapes.PASSAGE,
+        translate('SELECT * WHERE { ?person <http://own>/<http://species> ?species }')
+      )).toBeTruthy();
+    })
+
+    
+    it('does not handle group by queries yet', () => {
+      expect(doesShapeAcceptOperation(
+        Shapes.PASSAGE,
+        translate('SELECT ?p WHERE { ?p <http://own> ?a } GROUP BY ?p')
+      )).toBeFalsy();
+    })
+    it('does not handle property path yet', () => {
+      expect(doesShapeAcceptOperation(
+        Shapes.PASSAGE,
+        translate('SELECT * WHERE { ?person <http://own>+ ?species }')
+      )).toBeFalsy();
+    })
+
+    // TODO: Eventually, we would like to identify too large operations
+    //       to keep them on the smart-client. The trade-off being that
+    //       the Passage server indeed does additional computation, but
+    //       at least, the FILTERs comprising thousands of checks are not
+    //       doing round-trips in the network.
+    //       However, not sure it's possible with the Shapes as for now.
+    //       Possibly possible with the parser that should arrive in
+    //       Comunica 5.
+
+    
+    //     it('should not accept since the values is too large', () => {
+    //       expect(doesShapeAcceptOperation(
+    //         Shapes.PASSAGE,
+    //         (() => { const query = translate(`
+    // SELECT * WHERE {
+    // VALUES ?animal { "turtoise" "fox" "eagle" "dog" "snake" }
+    // ?person <http://own> ?animal
+    // }`); console.log(query.input); return query; })()
+    //       )).toBeFalsy()
+    //     })
+
+    
+    //     it('should not accept since the filter is too large', () => {
+    //       expect(doesShapeAcceptOperation(
+    //         Shapes.PASSAGE,
+    //         (() => { const query = translate(`
+    // SELECT * WHERE {
+    // ?person <http://own> ?animal
+    // FILTER (?animal != "turtoise" &&
+    // ?animal != "fox" &&
+    // ?animal != "eagle" &&
+    // ?animal != "dog" &&
+    // ?animal != "snake")
+    // }`); console.log(query); return query; })()
+    //       )).toBeFalsy();
+    //     })
+    
   })
 })
            
